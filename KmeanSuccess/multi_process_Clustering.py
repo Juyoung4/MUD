98da5294f0c9f1b1a8e87ef1ff9df3c0b7102a71
Hye_won <49637333+hhhhyewon@users.noreply.github.com>
2019-12-08 06:49:28 +0900

    Create multi_process_Clustering.py

---
+++ KmeanSuccess/multi_process_Clustering.py
@@ -0,0 +1,32 @@
+from Kmeans_success.K_class import *
+
+main_economy = run_kmeans('economy',15,30)
+main_it =run_kmeans('IT_science',10,20)
+main_society =run_kmeans('society',15,30)
+main_politics =run_kmeans('politics',15,25)
+
+mainlist = [main_economy,main_it,main_society,main_politics]
+# main_economy.play()
+# main_it.play()
+# main_society.play()
+# main_politics.play()
+
+from multiprocessing import Process
+#유동K가지고 멀티프로세싱 클러스터링
+def multi_Clustering_optimalK(mainlist):
+    for categorymain in mainlist:
+        print('category start')
+        proc = Process(categorymain.play())
+        proc.start()
+
+#고정K가지고 멀티프로세싱 클러스터링
+def multi_Clustering_fixedK(mainlist):
+    for categorymain in mainlist:
+        proc = Process(categorymain.play_fixK())
+        proc.start()
+
+
+
+
+multi_Clustering_optimalK(mainlist)
+'6:18분시작'
