207adba559a5f6462c460ca320e4680821267c38
Hye_won <49637333+hhhhyewon@users.noreply.github.com>
2019-12-08 20:52:14 +0900

    Create each_process_Clustering.py

---
+++ KmeanSuccess/each_process_Clustering.py
@@ -0,0 +1,13 @@
+from final_KmeansClass import *
+#
+# main_economy = run_kmeans('economy',15,30)
+# main_economy.play_fixK()
+
+main_it =run_kmeans('IT_science',10,20)
+main_it.play_fixK()
+
+# main_society =run_kmeans('society',15,30)
+# main_society.play_fixK()
+#
+# main_politics =run_kmeans('politics',15,25)
+# main_politics.play_fixK()
